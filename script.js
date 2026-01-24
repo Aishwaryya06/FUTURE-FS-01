48d5d44b818a6898336293230e118288d1dd5d44
Aishwaryya06 <aishwaryaji1227@gmail.com>
2026-01-24 21:18:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-function scrollToContact() {
-    document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
-}
-
-document.getElementById("contactForm").addEventListener("submit", function(e) {
+document.querySelector("form").addEventListener("submit", function(e) {
     e.preventDefault();
-    document.getElementById("successMsg").innerText =
-        "Message sent successfully! 😊";
-    this.reset();
+    alert("Message sent successfully!");
 });
